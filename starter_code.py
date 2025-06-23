f16a611a40df880e7606802ba23974a538a4b9e6
chrrr007 <chrrr007@gmail.com>
2025-06-23 18:21:32 +0100

    Renamed original PartOne to starter_code.py

--- starter_code.py
+++ starter_code.py
@@ -31,60 +31,11 @@ nlp.max_length = 2000000
 # Part One 1(a): Read the contents of each .txt file
 
 # def read_novels(path=Path.cwd() / "p1-texts" / "novels"):      -DONE!-
-def read_novels(path):
-    # def read_novels(path=Path(r'C:\Users\ClaudiaRoehn\Desktop\New folder\OneDrive - Home\0_NLP\p1-texts\novels')):
+# def read_novels(path):
+#     # def read_novels(path=Path(r'C:\Users\ClaudiaRoehn\Desktop\New folder\OneDrive - Home\0_NLP\p1-texts\novels')):
 
-    """Reads texts from a the specified directory of .txt files and returns a DataFrame with the text, title,
-    author, and year"""
-
-    # Create lists to store data for df
-    texts = []
-    titles = []
-    authors = []
-    years = []
-    
-    # Get all  .txt files in the cwd
-    txt_files = list(path.glob("*.txt"))
-    print(f"Files with any extension: {[f.name for f in txt_files]}")
-    #print(f"Found {len(txt_files)} .txt files: {[f.name for f in txt_files]}")
-
-    for file_path in txt_files:
-        try:
-            with open (file_path, 'r', encoding='utf-8') as file:
-                text_content = file.read()                            # Read entire file content
-            #print(f"Contents of {txt_file}:\n{content}\n")         # Print the content
-    
-            filename = file_path.stem   # removes .txt, keeps only part from path we need
-
-            parts = filename.split('-')
-
-            if len(parts) >= 3:
-                title = parts[0].strip().replace("_", " ")
-                author = parts[1].strip().replace("_", " ")
-                year_str = parts[2].strip().replace("_", " ")
-
-                # Convert year to integer so that we can sort
-                year = int(year_str)                
-
-                texts.append(text_content)
-                titles.append(title)
-                authors.append(author)
-                years.append(year)
-
-            else:
-                print(f"Error: Filename '{filename}' not in format (title-author-year)")
-            
-        except Exception as e:
-            print(f"Error processing file '{file_path}': {e}")
-    df = pd.DataFrame({
-        "text": texts,
-        "title": titles,
-        "author": authors,
-        "year": years
-    })    
-    
-    df = df.sort_values("year").reset_index(drop=True)          #re-sorting df to get required sorting by year
-    return df
+#     """Reads texts from a the specified directory of .txt files and returns a DataFrame with the text, title,
+#     author, and year"""
 
    
 #def nltk_ttr(text):            -DONE!- Part One 1.(b):
